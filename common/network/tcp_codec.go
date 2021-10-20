2dccfc0e273a9374ec86267c872be2ac6d24d35b
ouyang506 <ouyang506@163.com>
2021-10-20 20:56:06 +0800

    tmp commit

--- common/network/tcp_codec.go
+++ common/network/tcp_codec.go
@@ -1,96 +1,103 @@
 package network
 
-// import (
-// 	"errors"
+import (
+	"common/utility/ringbuffer"
+	"errors"
+)
 
-// 	"goserver/common/network/gnet"
-// )
+const (
+	max_frame_len = 2 * 1024 * 1024 // 2M
+)
 
-// const (
-// 	max_frame_len = 100000
-// )
+type TcpCodec struct {
+}
 
-// type TcpCodec struct {
-// }
+// Encode encodes frames upon server responses into TCP stream.
+func (cc *TcpCodec) Encode(buf []byte, ring *ringbuffer.RingBuffer) (err error) {
+	length := len(buf)
+	if length <= 0 {
+		return errors.New("encode tcp frame length error")
+	}
 
-// // Encode encodes frames upon server responses into TCP stream.
-// func (cc *TcpCodec) Encode(c gnet.Conn, buf []byte) (out []byte, err error) {
-// 	length := len(buf)
-// 	if length <= 0 {
-// 		return nil, errors.New("encode tcp frame length error")
-// 	}
+	if length > max_frame_len {
+		return errors.New("encode tcp frame length greater than max_frame_len")
+	}
 
-// 	if length > max_frame_len {
-// 		return nil, errors.New("encode tcp frame length greater than max_frame_len")
-// 	}
+	for {
+		b := byte(length & 0x7F)
+		length = length >> 7
+		if length > 0 {
+			ring.WriteByte(byte(0x80) | b)
+		} else {
+			ring.WriteByte(b)
+			break
+		}
+	}
+	ring.Write(buf)
+	return
+}
 
-// 	out = writeLength(length)
+// Decode decodes frames from TCP stream via specific implementation.
+func (cc *TcpCodec) Decode(c gnet.Conn) ([]byte, error) {
 
-// 	out = append(out, buf...)
-// 	return
-// }
+	in := c.Read()
+	if len(in) <= 0 {
+		return nil, errors.New("decode tcp frame length error")
+	}
 
-// // Decode decodes frames from TCP stream via specific implementation.
-// func (cc *TcpCodec) Decode(c gnet.Conn) ([]byte, error) {
+	frameLen, shift, succ := readLength(in)
+	if !succ {
+		return nil, nil
+	}
 
-// 	in := c.Read()
-// 	if len(in) <= 0 {
-// 		return nil, errors.New("decode tcp frame length error")
-// 	}
+	if frameLen <= 0 {
+		return nil, errors.New("decode tcp frame parse length error")
+	}
 
-// 	frameLen, shift, succ := readLength(in)
-// 	if !succ {
-// 		return nil, nil
-// 	}
+	if len(in) < shift+frameLen {
+		return nil, nil
+	}
 
-// 	if frameLen <= 0 {
-// 		return nil, errors.New("decode tcp frame parse length error")
-// 	}
+	fullMessage := make([]byte, frameLen)
+	copy(fullMessage, in[shift:shift+frameLen])
+	c.ShiftN(shift + frameLen)
+	return fullMessage, nil
+}
 
-// 	if len(in) < shift+frameLen {
-// 		return nil, nil
-// 	}
+func writeLength(length int) (out []byte) {
+	out = make([]byte, 0, 6)
+	for {
+		b := byte(length & 0x7F)
+		length = length >> 7
 
-// 	fullMessage := make([]byte, frameLen)
-// 	copy(fullMessage, in[shift:shift+frameLen])
-// 	c.ShiftN(shift + frameLen)
-// 	return fullMessage, nil
-// }
+		if length > 0 {
+			out = append(out, byte(0x80)|b)
+		} else {
+			out = append(out, b)
+			break
+		}
+	}
+	return
+}
 
-// func writeLength(length int) (out []byte) {
-// 	out = make([]byte, 0, 6)
-// 	for {
-// 		b := byte(length & 0x7F)
-// 		length = length >> 7
+func readLength(buf []byte) (out int, shift int, succ bool) {
+	out = 0
+	shift = 0
+	succ = false
+	for i, x := range buf {
+		shift++
+		out = (int(x)&0x7F)<<(7*i) | out
 
-// 		if length > 0 {
-// 			out = append(out, byte(0x80)|b)
-// 		} else {
-// 			out = append(out, b)
-// 			break
-// 		}
-// 	}
-// 	return
-// }
+		if out > max_frame_len {
+			succ = false
+			return
+		}
 
-// func readLength(buf []byte) (out int, shift int, succ bool) {
-// 	out = 0
-// 	shift = 0
-// 	succ = false
-// 	for i, x := range buf {
-// 		shift++
-// 		out = (int(x)&0x7F)<<(7*i) | out
+		if x&0x80 <= 0 {
+			succ = false
+			break
+		}
+	}
 
-// 		if out > max_frame_len {
-// 			succ = false
-// 			return
-// 		}
-
-// 		if x&0x80 <= 0 {
-// 			succ = false
-// 			break
-// 		}
-// 	}
-
-// 	return
-// }
+	return
+}
